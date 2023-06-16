fd9fa145a4f73ca8ade60c520ef53b0877db612f
Gizuzu <admin@gachi.su>
2023-06-16 16:33:25 +0300

    Add top display

--- src/scripts/constants/cities.js
+++ src/scripts/constants/cities.js
@@ -2,13 +2,16 @@ export const cities = [
   {
     tag: "ОКБ-М",
     color: "--blue-400",
+    colorDark: "--blue-900",
   },
   {
     tag: "ЦГБ-П",
     color: "--red-400",
+    colorDark: "--red-900",
   },
   {
     tag: "ЦГБ-Н",
     color: "--green-400",
+    colorDark: "--green-900",
   }
 ]
