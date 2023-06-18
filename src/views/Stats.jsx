8ec52c38bf96339293a25034a5345783f259f6a4
Gizuzu <admin@gachi.su>
2023-06-18 11:31:43 +0300

    Update Stats.jsx

--- src/views/Stats.jsx
+++ src/views/Stats.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Stats() {
-  return (<></>)
+  return (<>
+    <div className="w-12 h-full flex flex-column align-items-center justify-content-center">
+      <span className="font-bold text-2xl p-4 border-round-xl surface-card shadow-3">Скоро!</span>
+    </div>
+  </>)
 }
